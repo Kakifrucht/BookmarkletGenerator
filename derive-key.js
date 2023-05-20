29dfe56c0d9902b9443a691f6d767f7be743d5ad
Kakifrucht <prieto@wunderlich.pw>
2023-05-20 22:33:33 +0200

    Add delimiter

--- derive-key.js
+++ derive-key.js
@@ -1,5 +1,5 @@
 function deriveKey(password, salt, iterations = 10000) {
-    let hashedPassword = password + salt;
+    let hashedPassword = password + ':' + salt;
     for (let i = 0; i < iterations; i++) {
         hashedPassword = forge_sha256(hashedPassword);
     }
